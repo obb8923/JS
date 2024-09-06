1f83e6f95637d589ae4ef684e1dcd0cae51fb192
JeongBin Park <obb8923@naver.com>
2024-09-06 19:06:15 +0900

    CHORE: 주석

--- 1.canvas/app.js
+++ 1.canvas/app.js
@@ -1,9 +1,9 @@
-// create
+// canvas 생성
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
-
+// ctx생성
 const ctx = canvas.getContext("2d");
-// initialize
+// canvas 크기 설정
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -20,11 +20,11 @@ ctx.closePath();
 ctx.fillStyle = "rgb(0, 200,0)";
 ctx.fill();
 
-// circle
+// circle (반원)
 ctx.fillStyle = "yellow"; // 채우기색상
 ctx.strokeStyle = "red"; // 선색상
 ctx.beginPath();
-ctx.arc(400, 450, 100, 0, Math.PI, true); // 0~360°까지경로를만들기.
+ctx.arc(200, 300, 100, 0, Math.PI, true); // 0~360°까지경로를만들기.
 ctx.closePath(); // 경로닫기.
 ctx.fill(); // 색상채우기.
 ctx.stroke(); // 선그리기.
@@ -32,26 +32,26 @@ ctx.stroke(); // 선그리기.
 //ellipse
 ctx.strokeStyle = "red";
 ctx.beginPath();
-ctx.ellipse(200, 70, 80, 50, 0, 0, Math.PI * 2);
+ctx.ellipse(400, 70, 80, 50, 0, 0, Math.PI * 2);
 ctx.stroke();
 
-// 원으로 타원 그리기
+// 원을 그리는 방법으로 타원 그리기
 ctx.strokeStyle = "blue";
 ctx.scale(1, 0.7); //가로,세로 변형
 ctx.beginPath();
-ctx.arc(200, 150, 80, 0, Math.PI * 2, true);
+ctx.arc(600, 150, 80, 0, Math.PI * 2, true);
 ctx.stroke();
 
 //2차 베지에 곡선
 ctx.beginPath();
 ctx.moveTo(50, 100);
 ctx.quadraticCurveTo(200, 50, 350, 200);
+ctx.strokeStyle = "black";
 ctx.stroke();
 
 //3차 베지에 곡선
 ctx.beginPath();
 ctx.moveTo(50, 100);
-// 곡선의 시작 위치
 ctx.bezierCurveTo(90, 250, 310, 10, 350, 100);
 ctx.strokeStyle = "green";
 ctx.stroke();
